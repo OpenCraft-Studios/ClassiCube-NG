8b6218ea1757b450cc6c8124370082663dd75a07
C-777 <git@cirozdp.8shield.net>
2026-06-27 23:18:51 +0200

    ci(Linux): don't download libGL nor libX11

--- NewMakefile.mk
+++ NewMakefile.mk
@@ -1,9 +1,9 @@
 ## —— Project settings ————————————————————————————————————————————————————————
-  SRC := src
-  BUILD := /tmp/opencraft.linux
+  src := src
+  build := /tmp/opencraft.$(platform)
 
-  C_SOURCES := $(wildcard $(SRC)/*.c) # Main.c, Audio.c,...
-  OBJECTS := $(patsubst $(SRC)/%.c,$(BUILD)/%.c.o,$(C_SOURCES)) # Main.c.o,...
+  C_SOURCES := $(wildcard $(src)/*.c) # Main.c, Audio.c,...
+  OBJECTS := $(patsubst $(src)/%.c,$(build)/%.c.o,$(C_SOURCES)) # Main.c.o,...
 
 ## —— Toolchain settings ——————————————————————————————————————————————————————
   CC := cc
@@ -12,35 +12,36 @@
   MAKE = make
   MAKEFLAGS += -j$(shell nproc) --no-print-directory
   
-  CFLAGS  += -pipe -fno-math-errno -Werror -Wno-error=missing-braces -Wno-error=strict-aliasing
-  LDFLAGS += -lX11 -lXi -lpthread -ldl -lm -lGL -rdynamic
+  CFLAGS  += -pipe -fno-math-errno
+  LDFLAGS += -no-pie
+  LIBS    += 
 
+-include config.mk
 
-
-EXECUTABLE := OpenCraft
-$(EXECUTABLE): $(OBJECTS) $(BUILD)/libbearssl.a ## Makes the final game
+target := OpenCraft
+$(target): $(OBJECTS) $(build)/libbearssl.a ## Makes the final game
 	@printf "  $(BLUE)Linking the game...$(RESET)\n"
-	@$(LD) $^ -o $@ $(LDFLAGS)
+	@$(LD) $(LDFLAGS) $^ -o $@ $(LIBS)
 	@chmod +x $@
 
 
-$(BUILD)/libbearssl.a: ## Builds BearSSL (needed for internet connection)
+$(build)/libbearssl.a: ## Builds BearSSL (needed for internet connection)
     # Delegates BearSSL build to its own Makefile
     # `TARGET` represents the name of the resultant file
 	$(MAKE) -Cthird_party/bearssl TARGET=$@
 
 
 
-$(BUILD)/%.c.o: $(SRC)/%.c | $(BUILD) ## Rule to compile C files
+$(build)/%.c.o: $(src)/%.c | $(build) ## Rule to compile C files
 	@printf "  $(GREEN)Compiling$(RESET) %s\n" "$(@F)"
 	@$(CC) $(CFLAGS) -c $< -o $@
 
 
-$(BUILD): ## Create the build directory in case it doesn't exist
-	mkdir -p $(BUILD)
+$(build): ## Create the build directory in case it doesn't exist
+	mkdir -p $(build)
 
 clean:
-	@rm -rf $(BUILD) $(EXECUTABLE)
+	@rm -rf $(build) $(target)
 	@$(MAKE) -Cthird_party/bearssl clean
 
 -include misc/colors.mk
