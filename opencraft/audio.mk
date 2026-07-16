1d471d647bbb0e59a7e4d308a53e01230853abd3
C-777 <git@cirozdp.8shield.net>
2026-07-16 23:28:27 +0200

    feat: add Makefile and USE flags

---
+++ opencraft/audio.mk
@@ -0,0 +1,23 @@
+AUD_BACKENDS := openal os2 sles winmm
+AUD_USE       = $(filter $(AUD_BACKENDS), $(USE))
+AUD_OBJS     := $(BUILD_DIR)/Audio.c.o $(BUILD_DIR)/Audio_$(AUD_USE).c.o
+
+ifeq ($(AUD_USE), )
+	$(error Please, select an audio backend or disable libaudio (make ... AUDIO=0))
+else ifeq ($(AUD_USE), openal)
+	AUD_CFLAGS += -DCC_AUD_BACKEND=CC_AUD_BACKEND_OPENAL
+	AUD_LDLIBS += -ldl
+else ifeq ($(AUD_USE), os2)
+	AUD_CFLAGS += -DCC_AUD_BACKEND=CC_AUD_BACKEND_OS2
+else ifeq ($(AUD_USE), sles)
+	AUD_CFLAGS += -DCC_AUD_BACKEND=CC_AUD_BACKEND_OPENSLES
+else ifeq ($(AUD_USE), winmm)
+	AUD_CFLAGS += -DCC_AUD_BACKEND=CC_AUD_BACKEND_WINMM
+endif
+
+$(BUILD_DIR)/Audio_%.c.o: $(SRC_DIR)/Audio_%.c | $(BUILD_DIR)
+	$(CC) $(CFLAGS) $(AUD_CFLAGS) -c $< -o $@
+
+override OBJS   += $(AUD_OBJS)
+override CFLAGS += $(AUD_CFLAGS)
+override LDLIBS += $(AUD_LDLIBS)
---
+++ opencraft/certs.mk
@@ -0,0 +1,18 @@
+CRT_BACKENDS := windows android openssl apple-sec
+CRT_USE       = $(filter $(CRT_BACKENDS), $(USE))
+
+ifeq ($(CRT_USE), )
+	override CRT_USE = openssl
+endif
+
+ifeq ($(CRT_USE), windows)
+	CRT_CFLAGS += -DCC_CRT_BACKEND=CC_CRT_BACKEND_WINCRYPTO
+else ifeq ($(CRT_USE), android)
+	CRT_CFLAGS += -DCC_CRT_BACKEND=CC_CRT_BACKEND_ANDROID
+else ifeq ($(CRT_USE), openssl)
+	CRT_CFLAGS += -DCC_CRT_BACKEND=CC_CRT_BACKEND_OPENSSL
+else ifeq ($(CRT_USE), apple-sec)
+	CRT_CFLAGS += -DCC_CRT_BACKEND=CC_CRT_BACKEND_APPLESEC
+endif
+
+override CFLAGS += $(CRT_CFLAGS)
