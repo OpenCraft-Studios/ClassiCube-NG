83ee9d8443dc525b4a47dfbea4ee36f9bc5c1832
C-777 <git@cirozdp.8shield.net>
2026-07-17 21:39:50 +0200

    feat(build): use more understandable variable names in gmake

--- opencraft/opengfx.mk
+++ opencraft/opengfx.mk
@@ -1,39 +1,39 @@
-GFX_BACKENDS := gl1 gl2 gl11 softgpu softmin softfp d3d9 d3d11
-GFX_USE       = $(filter $(GFX_BACKENDS), $(USE))
-GFX_OBJS     := $(BUILD_DIR)/Graphics_$(GFX_USE).c.o
+override GFX_BACKENDS := gl1 gl2 gl11 softgpu softmin softfp d3d9 d3d11
+gfx.USE   = $(filter $(GFX_BACKENDS),$(USE))
+gfx.OBJS := $(oc.build)/Graphics_$(gfx.USE).c.o
 
-ifeq ($(GFX_USE), )
+ifeq ($(gfx.USE),)
 $(error Please, select a graphic backend or disable OpenGFX (make ... OPENGFX=0))
-else ifeq ($(GFX_USE), gl1)
+else ifeq ($(gfx.USE),gl1)
 GFX_IS_OPENGL = 1
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_GL1
-else ifeq ($(GFX_USE), gl2)
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_GL1
+else ifeq ($(gfx.USE),gl2)
 GFX_IS_OPENGL = 1
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_GL2
-else ifeq ($(GFX_USE), gl11)
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_GL2
+else ifeq ($(gfx.USE),gl11)
 GFX_IS_OPENGL = 1
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_GL11
-GFX_CFLAGS    += -DCC_BUILD_GL11
-else ifeq ($(GFX_USE), softgpu)
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_SOFTGPU
-else ifeq ($(GFX_USE), softmin)
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_SOFTMIN
-else ifeq ($(GFX_USE), softfp)
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_SOFTFP
-else ifeq ($(GFX_USE), d3d9)
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_D3D9
-else ifeq ($(GFX_USE), d3d11)
-GFX_CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_D3D11
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_GL11
+gfx.CFLAGS    += -DCC_BUILD_GL11
+else ifeq ($(gfx.USE),softgpu)
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_SOFTGPU
+else ifeq ($(gfx.USE),softmin)
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_SOFTMIN
+else ifeq ($(gfx.USE),softfp)
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_SOFTFP
+else ifeq ($(gfx.USE),d3d9)
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_D3D9
+else ifeq ($(gfx.USE),d3d11)
+gfx.CFLAGS    += -DCC_GFX_BACKEND=CC_GFX_BACKEND_D3D11
 endif
 
 ifdef GFX_IS_OPENGL
-GFX_LDLIBS += $(shell $(PKGCONF) --libs gl) -ldl
-GFX_CFLAGS += $(shell $(PKGCONF) --cflags gl)
+gfx.LDLIBS += $(shell $(PKGCONF) --libs gl) -ldl
+gfx.CFLAGS += $(shell $(PKGCONF) --cflags gl)
 endif
 
-$(BUILD_DIR)/Graphics_%.c.o: $(SRC_DIR)/Graphics_%.c | $(BUILD_DIR)
-	$(CC) $(CFLAGS) $(GFX_CFLAGS) -c $< -o $@
+$(oc.build)/Graphics_%.c.o: $(oc.src)/Graphics_%.c | $(oc.build)
+	$(CC) $(CFLAGS) $(gfx.CFLAGS) -c $< -o $@
 
-override OBJS   += $(GFX_OBJS)
-override LDLIBS += $(GFX_LDLIBS)
-override CFLAGS += $(GFX_CFLAGS)
+override OBJS   += $(gfx.OBJS)
+override LDLIBS += $(gfx.LDLIBS)
+override CFLAGS += $(gfx.CFLAGS)
