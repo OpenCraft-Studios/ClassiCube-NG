f94197a5533f966de67fad3875491296a16d0a23
C-777 <git@cirozdp.8shield.net>
2026-07-18 16:57:35 +0200

    refactor(build): decorate with comments

--- bearssl/module.mk
+++ bearssl/module.mk
@@ -5,7 +5,7 @@ bearssl.OBJS   := $(patsubst $(bearssl.src)/%.c,$(bearssl.build)/%.c.o,$(bearssl
 
 bearssl.CFLAGS += -DCC_SSL_BACKEND=CC_SSL_BACKEND_BEARSSL
 
-$(bearssl.build)/%.c.o: bearssl/%.c | $(bearssl.build)
+$(bearssl.build)/%.c.o: $(bearssl.src)/%.c | $(bearssl.build)
 	$(call log_compile,$(<F),bearssl,$(VERSION))
 	$(CC) $(CFLAGS) $(bearssl.CFLAGS) -c $< -o $@
 
